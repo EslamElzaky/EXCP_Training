69b8a79af1ec0d7b8477f6ad0a9c22b55b027521
Eslamelzaky <132919274+EslamElzaky@users.noreply.github.com>
2025-06-21 03:06:19 +0300

    finish login page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -1,68 +1,157 @@
 import 'package:excp_training/constant.dart';
 import 'package:excp_training/helper/custom_button.dart';
+import 'package:excp_training/helper/custom_snack_bar.dart';
 import 'package:excp_training/helper/custom_text_field.dart';
 import 'package:excp_training/views/home_view.dart';
-import 'package:excp_training/views/regester_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
   static String id = 'login_page';
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLoading = false;
+
+  GlobalKey<FormState> formkey = GlobalKey();
+  String? email, password;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: kPrimarycolor,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CostumFormTextField(
-                hintText: 'email',
-                obscureText: false,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CostumFormTextField(
-                hintText: 'password',
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              CustomButton(
-                size: double.infinity,
-                text: 'Login',
-                onTap: () {
-                  Navigator.pushNamed(context, 'BottomNavigation');
-                  // naHandle login action
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0),
-                child: Row(
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        backgroundColor: kPrimarycolor,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Form(
+            key: formkey,
+            child: ListView(
+              children: [
+                SizedBox(
+                  height: 75,
+                ),
+                // Image.asset(
+                //   'assets/images/scholar.png',
+                //   height: 100,
+                // ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Text('Don\'t have an account?',
-                        style: TextStyle(color: Colors.white)),
+                    Text(
+                      'Scholer Chat',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: 32,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 75,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Log In',
+                      style: TextStyle(fontSize: 24, color: Colors.white),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                CostumFormTextField(
+                  onChanged: (data) {
+                    email = data;
+                  },
+                  hintText: 'Email',
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                CostumFormTextField(
+                  obscureText: true,
+                  onChanged: (data) {
+                    password = data;
+                  },
+                  hintText: 'password',
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                CustomButton(
+                  size: double.infinity,
+                  onTap: () async {
+                    if (formkey.currentState!.validate()) {
+                      isLoading = true;
+                      setState(() {});
+                      try {
+                        await logInUsers();
+                        if (!context.mounted) return;
+
+                        showSnackBar(context, '✅ Login successful');
+                        Navigator.pushNamed(context, HomeView.id,
+                            arguments: email);
+                      } on FirebaseAuthException catch (e) {
+                        if (e.code == 'user-not-found') {
+                          showSnackBar(
+                              context, '❌ No user found with this email.');
+                        } else if (e.code == 'wrong-password') {
+                          showSnackBar(context, '🔐 Incorrect password.');
+                        } else if (e.code == 'invalid-email') {
+                          showSnackBar(
+                              context, '📧 The email address is invalid.');
+                        } else if (e.code == 'too-many-requests') {
+                          showSnackBar(context,
+                              '🚫 Too many failed attempts. Try again later.');
+                        } else {
+                          showSnackBar(
+                              context, '⚠️ An error occurred: ${e.message}');
+                        }
+                      }
+
+                      isLoading = false;
+                      setState(() {});
+                    }
+                  },
+                  text: 'LOGIN',
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'don\'t have an account ?',
+                      style: TextStyle(color: Colors.white),
+                    ),
                     GestureDetector(
                       onTap: () {
-                        Navigator.pushNamed(context, RegesterPage.id);
+                        Navigator.pushNamed(context, 'regesterpage');
                       },
                       child: Text(
-                        ' Register',
+                        '  Register',
                         style: TextStyle(color: Color(0xffC7EDE6)),
                       ),
                     )
                   ],
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Future<void> logInUsers() async {
+    UserCredential user = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email!, password: password!);
   }
 }
