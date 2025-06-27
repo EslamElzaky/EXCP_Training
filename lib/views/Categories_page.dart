969bae4f8d87e6ac41752dc3af09826779367a5d
Eslamelzaky <132919274+EslamElzaky@users.noreply.github.com>
2025-06-27 11:10:48 +0300

    Categories_page

---
+++ lib/views/Categories_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoriesPage extends StatelessWidget {
+  const CategoriesPage({super.key});
+static String id = 'categories_page';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
