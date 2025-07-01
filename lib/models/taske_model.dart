5a882ff419b262521b725fd3a99f09df75e603b9
Eslamelzaky <132919274+EslamElzaky@users.noreply.github.com>
2025-07-01 10:07:50 +0300

    generat model

--- lib/models/taske_model.dart
+++ lib/models/taske_model.dart
@@ -1,8 +1,19 @@
-class TaskeModel {
+import 'package:hive/hive.dart';
+
+part 'taske_model.g.dart';
+
+@HiveType(typeId: 0)
+
+class TaskeModel extends HiveObject {
+  @HiveField(0)
   final String typeTask;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String content;
+  @HiveField(3)
   final String date;
+  @HiveField(4)
   final int color;
 
   TaskeModel({
